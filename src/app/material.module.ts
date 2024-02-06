ad4286a12f9c34f8834496e945083576a04e9f54
Irina Dimcheva <dimchevairina@gmail.com>
2024-02-06 23:21:41 +0200

    Updated angular to @17, changed angular material legacy imports

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
+import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
-import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
-import { MatLegacyOptionModule as MatOptionModule } from "@angular/material/legacy-core";
-import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
-import { MatLegacyPaginatorModule as MatPaginatorModule } from '@angular/material/legacy-paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   imports: [
@@ -26,7 +26,7 @@ import { MatLegacyPaginatorModule as MatPaginatorModule } from '@angular/materia
     MatSelectModule,
     MatOptionModule,
     MatDialogModule,
-    MatPaginatorModule
+    MatPaginatorModule,
   ],
   exports: [
     MatToolbarModule,
@@ -40,7 +40,7 @@ import { MatLegacyPaginatorModule as MatPaginatorModule } from '@angular/materia
     MatSelectModule,
     MatOptionModule,
     MatDialogModule,
-    MatPaginatorModule
-  ]
+    MatPaginatorModule,
+  ],
 })
-export class MaterialModule { }
+export class MaterialModule {}
